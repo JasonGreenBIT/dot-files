7ee4474918c820d18fbf9f8c3c056c6db752ee02
Jason Green <jason@jasongreenbit.com>
2020-09-20 12:51:48 +1000

    Add Python Virtualenv to Bash Prompt

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,4 +1,5 @@
 BOLD="\[\e[1m\]"
+FG_BLUE="\[\e[34m\]"
 FG_GREEN="\[\e[32m\]"
 FG_LIGHT_CYAN="\[\e[96m\]"
 FG_LIGHT_GREEN="\[\e[92m\]"
@@ -14,9 +15,15 @@ function __set_ps1 {
 		PROMPT_COLOR="$FG_LIGHT_RED"
 	fi
 
-	TERM_TITLE="\[\e]0;\u@\h:\w\a\]"
-	PS1_PRE="$TERM_TITLE$BOLD$FG_LIGHT_CYAN$(date +"%F %T") $FG_LIGHT_MAGENTA\u@\h $FG_LIGHT_YELLOW\w"
-	PS1_POST="\n$PROMPT_COLOR\! \$$NO_COLOR "
+	if [ -n "$VIRTUAL_ENV" ]; then
+		PS1_VENV=" ($VIRTUAL_ENV)"
+	else
+		PS1_VENV=""
+	fi
+
+	TERM_TITLE="\[\e]0;\u@\h:\w$PS1_VENV\a\]"
+	PS1_PRE="$TERM_TITLE$BOLD$FG_LIGHT_CYAN$(date +"%F %T") $FG_LIGHT_MAGENTA\u@\h $FG_LIGHT_YELLOW\w$NO_COLOR"
+	PS1_POST="$FG_BLUE$PS1_VENV\n$BOLD$PROMPT_COLOR\! \$$NO_COLOR "
 	PS1="$PS1_PRE$PS1_POST"
 }
 
@@ -28,8 +35,10 @@ if [ -f ~/src/dot-files/git/prompt.sh ]; then
 	GIT_PS1_SHOWUNTRACKEDFILES=true
 	GIT_PS1_SHOWUPSTREAM="auto"
 
-	PS1_GIT="$NO_COLOR $FG_GREEN($NO_COLOR%s$FG_GREEN)$NO_COLOR"
+	PS1_GIT=" $FG_GREEN($NO_COLOR%s$FG_GREEN)$NO_COLOR"
 	PROMPT_COMMAND='__set_ps1 && __git_ps1 "$PS1_PRE" "$PS1_POST" "$PS1_GIT"'
 else
 	PROMPT_COMMAND="__set_ps1"
 fi
+
+export VIRTUAL_ENV_DISABLE_PROMPT=1
